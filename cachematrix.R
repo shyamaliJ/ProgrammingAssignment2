cf4266ffc023374da35bb55de25caa7879019e5f
SamJ <shya.iyer@gmail.com>
2020-04-28 15:08:33 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,38 @@
 
-## The function takes input x as matrix and the matrix is assigned as y in the environment inside the 
-## set function and m is reset to NULL.
+
+## This function takes input and creates variable for inverting matrix. For new inputs, 
+## this function also resets inv variable to NULL to prevent wrong ca
 
 makeCacheMatrix <- function(x = matrix()) { 
-  m <- NULL
+  ## The function takes input x as matrix and inv variable is set to NULL
+  inv <- NULL
   set <- function(y) {
   x <<- y
-  m <<- NULL
+  inv <<- NULL
 }
-get <- function(){x}
-setmean <- function(solve) {m <<- solve}
-getmean <- function() {m}
+get <- function() x ##function returns value of input, x
+setinverse <- function(solve) {inv <<- solve} ##function sets and returns value of inv
+getinverse <- function() {inv} ## function gets and returns value of inv 
 list(set = set, get = get,
-     setmean = setmean,
-     getmean = getmean)
-
+     setinverse = setinverse,
+     getinverse = getinverse)
+## output of this function is a list so that it can be calles using $ symbol later
 }
-
 
 
 ## the function tries to get inverse of matrix from list. If it gets it, it prints it.If not,
 ## it calculates the inverse by getting input matrix from 'get' and setting the calculated inverse
-## to 'setmean'
+## to 'setinverse'
 
 cacheSolve <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
+  inv <- x$getinverse() ## gets the inverse value from getinverse of prev. function
+  if(!is.null(inv)) {
     message("getting cached data")
-    return(m)
+    return(inv) ## If inv is not equal to NULL, then return the value with a message
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setmean(m)
-  m
-} ## Return a matrix that is the inverse of 'x'
+  data <- x$get() ##when inv=NULL get input value x
+  inv <- solve(data, ...) ## solve to get inverse of x
+  x$setinverse(inv) ## set value of inverse and return inverted matrix
+  inv
+} 
 
